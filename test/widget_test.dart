26f03b0f255e7fe093e7bdf2fb778b6f48871346
Dniam07 <gamingopan123@gmail.com>
2024-04-21 17:53:39 +0700

    first commit

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+void main() {
+  var name = "WILDAN ARYA";
+  var umur = 17;
+  var data = [1, 2, 3];
+  var marray = {"nama": name, "data": data};
+  print(marray);
+  print("NAMA SAYA : $name DAN UMUR SAYA : $umur");
+}
